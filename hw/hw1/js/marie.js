743372ea624df0c873cd8fa4d3a87d6d533bbe7d
marietaoko <123906703+marietaoko@users.noreply.github.com>
2023-03-27 16:39:19 -0400

    Add files via upload

--- hw/hw1/js/marie.js
+++ hw/hw1/js/marie.js
@@ -8,17 +8,10 @@ const close = document.querySelector('#closeMenu');
 const nav = document.querySelector('#myNav');
 
 
-// open.onclick = function() {
-//   nav.style.height = '30vh';
-// };
-
 open.addEventListener('click', function(){
   nav.style.height = '20vh';
 });
 
-// close.onclick = function(){
-//   nav.style.height = '0';
-// };
 close.addEventListener('click', function(){
   nav.style.height = '0';
 });
@@ -29,15 +22,8 @@ console.log(offset);
 marie.style.bottom = offset * 1.4 + 'px';
 france.style.bottom = offset * 0.3 + 'px';
 plain.style.transform = `translateX(-${offset * 0.4}px)`;
-
-// astronaut.style.transform =  "rotate(" + offset * 0.2 + "deg)";
-//astronaut.style.transform = 'translateY(' + '-' + offset * 0.5 + 'px)';
-//astronaut.style.transform = `translate(${offset * 0.2}px, -${offset * 0.5}px) rotate(${offset * 0.02}deg)`;
-// astronaut.style.transform = `translateY(-${offset * 0.5}px) rotate(${offset * 0.02}deg)`;
-// higher % scrolls faster
  });
 
  dart.addEventListener('click', function(){
-    // this.classList.add('change');
       this.classList.toggle('change');
   });
